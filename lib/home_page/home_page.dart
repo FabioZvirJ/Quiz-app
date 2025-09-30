ea8c3c5959ac46c979cfe7977c244fbbb47ee6d3
Fabio RIcardo Zvir Junior <zvirfabiojr88@gmail.com>
2025-09-29 21:58:08 -0300

    FEAT: adicionando text button

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -12,14 +12,28 @@ class HomePage extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 120, horizontal: 0),
             child: Column(
-              children: [Image.asset('assets/images/quiz-logo.png', width: 250),
-              SizedBox(height: 50,),
-              Text(
-                'Learn Flutter the fun way!', style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
+              children: [
+                Image.asset('assets/images/quiz-logo.png', width: 250),
+                SizedBox(height: 50),
+                Text(
+                  'Learn Flutter the fun way!',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
                 ),
-              ),],
+                SizedBox(height: 30),
+                OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(0),
+                    ),
+                    side: BorderSide(color: const Color.fromARGB(38, 0, 0, 0))
+                  ),
+                  child: Text(
+                    'Start Quiz',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
