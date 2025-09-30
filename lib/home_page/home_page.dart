d64ef5c239a5a8773730c951c24da31f27e5286a
Fabio RIcardo Zvir Junior <zvirfabiojr88@gmail.com>
2025-09-29 21:39:26 -0300

    FEAT: add text and rectify image

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -7,9 +7,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(color: Color.fromARGB(255, 113, 13, 228)),
-                child: Column(
-          children: [Image.asset('assets/images/quiz-logo.png')],
+        decoration: BoxDecoration(color: Color.fromARGB(255, 74, 7, 150)),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 120, horizontal: 0),
+            child: Column(
+              children: [Image.asset('assets/images/quiz-logo.png', width: 250),
+              SizedBox(height: 50,),
+              Text(
+                'Learn Flutter the fun way!', style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),],
+            ),
+          ),
         ),
       ),
     );
